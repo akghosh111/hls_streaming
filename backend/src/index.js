e5f6382f2af7e7357c17492add5d38f7a441fec6
akghosh111 <ghoshkiran2017@gmail.com>
2025-05-27 14:49:16 +0530

    added uploader middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@
 import express from 'express'
 import cors from 'cors'
 import { v4 as uuid } from 'uuid'
-// import { uploader } from './middlewares/uploader.js'
+import { uploader } from './middlewares/uploader.js'
 
 
 const port = process.env.PORT || 2000
@@ -18,10 +18,24 @@ app.use(express.json())
 
 app.use(express.urlencoded({ extended: false }))
 
-app.post('/api/upload', (req, res) => {
-    console.log("called");
-    return res.json({
-        data: "hello world"
+app.post('/api/upload', uploader('video'), (req, res) => {
+     if (!req.file) {
+        
+        return res.status(400).send('Video not sent!')
+    }
+
+    
+    const videoId = uuid()
+
+    
+    const uploadedVideoPath = req.file.path
+
+    
+    return res.status(200).json({ 
+        success: true, 
+        message: 'Video uploaded successfully',
+        videoId: videoId,
+        filePath: uploadedVideoPath
     })
 })
 
